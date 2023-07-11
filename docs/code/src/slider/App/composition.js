338287809bbd25d05284091c32a3ebc57457cf40
宋承亮3 <1422254429@qq.com>
2023-07-11 13:31:43 +0800

    update slider

--- docs/code/src/slider/App/composition.js
+++ docs/code/src/slider/App/composition.js
@@ -1,3 +1,33 @@
 import { ref, onUnmounted, onMounted } from 'vue'
-
+const pointer = ref(null);
+const track = ref(null);
 const value = ref(0)
+const currentState = ref(0);
+let fatherDom = {};
+
+  
+ const handleMouseUp = () => {
+   window.removeEventListener('mousemove', handleMouseMove)
+   
+}
+
+const handleMouseDown = (e) => {
+  window.addEventListener('mouseup', handleMouseUp)
+  window.addEventListener('mousemove', handleMouseMove)
+}
+
+const handleMouseMove = (e) => {
+  console.log(e.x)
+  currentState.value = e.x;
+  let temp = Math.ceil(( 100 * currentState.value - fatherDom.x) / fatherDom.width)
+  if (temp > 100) {
+    temp = 100;
+  } else if (temp < 0) {
+    temp = 0;
+  }
+  value.value = temp;
+}
+onMounted(() => {
+  pointer.value.addEventListener('mousedown', handleMouseDown)
+  fatherDom = track.value.getBoundingClientRect();
+})
